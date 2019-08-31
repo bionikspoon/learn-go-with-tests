582b338d1de56c4d248d92134bc8e86b1e2629e0
Manu Phatak <manu.phatak@procore.com>
2019-08-30 20:11:49 -0700

    update package structure

--- poker/cmd/webserver/main.go
+++ poker/cmd/webserver/main.go
@@ -3,16 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/bionikspoon/learn-go-with-tests/poker"
 )
 
-type PlayerStore interface {
-	GetPlayerScore(name string) int
-	RecordWin(name string)
-	GetLeague() Players
-}
-
 func main() {
-	server := NewPlayerServer(NewInMemoryPlayerStore())
+	server := poker.NewPlayerServer(poker.NewInMemoryPlayerStore())
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Printf("Could not listen on port 5000 %v", err)
